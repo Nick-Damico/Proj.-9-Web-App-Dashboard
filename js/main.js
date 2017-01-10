53b697b1b97902c53902915c7c4a100ddfb5887d
Nicholas D'Amico <nickalan82@icloud.com>
2017-01-10 12:06:21 -0500

    Fixed Nav,Added Social-Icons

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-// Author: Nicholas D'Amico
+// Author: Nicholas D"Amico
 // Contact: nickalan82@icloud.com
 // Checked project in: Chorme 54.0.02840.71, FireFox 50.0 & 49.0.1, safari 10.0.1
 // Treehouse Project 9: Web App Dashboard
 
-// ToDo:
-  // 1. Add message response to users click on 'save' btn in settings widget.
-  // 2. refactor Alert Msg.
-
-// iife function
+// jQuery Ready();
 $(function() {
 
     /////////////////////////////////////////////////////
@@ -17,11 +13,11 @@ $(function() {
     //  Alert Notification Close Btn.
 
     // Line Chart Canvas El.
-    var Linectx = document.getElementById('myLineChart').getContext("2d");
+    var Linectx = document.getElementById("myLineChart").getContext("2d");
     // Bar Chart Canvas El.
-    var Barctx = document.getElementById('myBarChart').getContext("2d");
+    var Barctx = document.getElementById("myBarChart").getContext("2d");
     // Doughnut Chart Canvas El.
-    var Piectx = document.getElementById('myPieChart').getContext("2d");
+    var Piectx = document.getElementById("myPieChart").getContext("2d");
     //  Hides Modal window on load.
     $(".form-confirm-msg").hide();
 
@@ -29,31 +25,31 @@ $(function() {
 
 
     //  input type="checkbox" #emailNotification
-    var $emailCheckbox = $('#emailNotification');
+    var $emailCheckbox = $("#emailNotification");
     //  input type="checkbox" #profilePrivacy
-    var $privacyCheckbox = $('#profilePrivacy');
+    var $privacyCheckbox = $("#profilePrivacy");
 
 
     /////////////////////////////////////////////////////
     // Functions
     /////////////////////////////////////////////////////
 
-    //  Appends msg to Modal and displays over 2000ms to user once send is 'clicked'.
+    //  Appends msg to Modal and displays over 2000ms to user once send is "clicked".
     function displayModal(msg) {
-        var $container = $('.form-confirm-msg');
+        var $container = $(".form-confirm-msg");
         $container.prepend(msg)
             .fadeIn()
             .delay(2500)
             .fadeOut(function(e) {
                 $(this).text("");
-            })
+            });
     }
 
     //  Sets Select option for users time zone in localStorage
-    // if localStorage.timeZone is 'undefined', select defaults to '<option>Select timeZone'
+    // if localStorage.timeZone is "undefined", select defaults to "<option>Select timeZone"
     function usersTimeZone( usersTime ) {
-        $('#timezone-select option').each(function() {
-            if ($(this).attr('value') === usersTime) {
+        $("#timezone-select option").each(function() {
+            if ($(this).attr("value") === usersTime) {
                 $(this).attr("selected", "true");
             }
         });
@@ -66,35 +62,37 @@ $(function() {
 
     var alertMsgs = [
       {
-        msg : 'You have 2 New Likes on Your recent post',
+        msg : "You have 2 New Likes on Your recent post"
       },
       {
-        msg : 'You have a New Message from Ryan Carson',
+        msg : "You have a New Message from Ryan Carson"
       }
     ];
 
     // REMOVES ALERT MSG FROM PAGE ON CLICK
-    $(document).on('click', '.close-alert', function() {
+    $(document).on("click", ".close-alert", function() {
         $this = $(this);
         $this.parent().fadeOut(1000);
     });
 
-    $('#alertLink').on('click', function(e) {
-      var $userAlertBox = $('.user-alert-inner-container');
+    $("#alertLink").on("click", function(e) {
+      var $userAlertBox = $(".user-alert-inner-container");
       $userAlertBox.show();
-      var msg = '';
+      var msg = "";
       for( var i = 0; i < alertMsgs.length; i++ ) {
         if( alertMsgs[i].msg !== "" ) {
-        msg += '';
-        msg += '<p class="user-alert-message">';
-        msg += '<strong>Alert </strong>';
-        msg += alertMsgs[i].msg + '!';
-        msg += '<span class="close-alert">x</span>';
-        msg += '</p>';
+        msg += "";
+        msg += "<p class='user-alert-message'>";
+        msg += "<strong>Alert </strong>";
+        msg += alertMsgs[i].msg + "!";
+        msg += "<span class='close-alert'>x</span>";
+        msg += "</p>";
         $userAlertBox.append(msg);
         msg = "";
         alertMsgs[i].msg = "";
-        }
+      } else {
+        $(".user-alert-container section").hide();
+      }
       }
     });
 
@@ -118,25 +116,25 @@ $(function() {
 
 
     //  CLICK EVENTS FOR TRAFFIC DATA
-    $('#hourlyData').on('click', function(e) {
+    $("#hourlyData").on("click", function(e) {
       e.preventDefault();
       myChart.data.datasets[0].data = hourlyChartData;
       myChart.update();
     });
 
-    $('#dailyData').on('click', function(e) {
+    $("#dailyData").on("click", function(e) {
       e.preventDefault();
       myChart.data.datasets[0].data = dailyChartData;
       myChart.update();
     });
 
-    $('#weeklyData').on('click', function(e) {
+    $("#weeklyData").on("click", function(e) {
       e.preventDefault();
       myChart.data.datasets[0].data = weeklyChartData;
       myChart.update();
     });
 
-    $('#monthlyData').on('click', function(e) {
+    $("#monthlyData").on("click", function(e) {
       e.preventDefault();
       myChart.data.datasets[0].data = monthlyChartData;
       myChart.update();
@@ -147,18 +145,18 @@ $(function() {
     //  LINE CHART CONSTRUCTOR
     //////////////////////////////////
     var myChart = new Chart(Linectx, {
-        type: 'line',
+        type: "line",
         data: {
             labels: ["16-22", "22-39", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
             datasets: [{
                 data: hourlyChartData, // DATA UPDATE ON CLICK EVENTS
                 lineTension: 0,
-                pointBorderColor: 'rgba(150, 132, 208, 1)',
+                pointBorderColor: "rgba(150, 132, 208, 1)",
                 pointRadius: 4,
                 pointBorderWidth: 2,
-                pointBackgroundColor: '#fff',
-                backgroundColor: 'rgba(150, 132, 208, 0.4)',
-                borderColor: 'rgba(150, 132, 208, 1)',
+                pointBackgroundColor: "#fff",
+                backgroundColor: "rgba(150, 132, 208, 0.4)",
+                borderColor: "rgba(150, 132, 208, 1)",
                 borderWidth: 2
             }]
         }, // CHART DATA VARIABLES ADDED HERE
@@ -177,23 +175,23 @@ $(function() {
     /////////////////////////////////////////////////////
 
     var myBarChart = new Chart(Barctx, {
-        type: 'bar',
+        type: "bar",
         data: {
             labels: ["S", "M", "T", "W", "T", "F", "S"],
             datasets: [
               {
-                label: 'Desktop Users',
+                label: "Desktop Users",
                 data: [50, 75, 175, 125, 225, 180, 100],
-                backgroundColor: 'rgba(94, 98, 179, 1)',
-                borderColor: 'rgba(94, 98, 179, 1)',
+                backgroundColor: "rgba(94, 98, 179, 1)",
+                borderColor: "rgba(94, 98, 179, 1)",
                 borderWidth: 1
             },
             {
-              type: 'bar',
-                label: 'Mobile Users',
+              type: "bar",
+                label: "Mobile Users",
                 data: [150, 95, 205, 245, 255, 210, 160],
-                backgroundColor: 'rgba(101, 162, 178, 1)',
-                borderColor: 'rgba(101, 162, 178, 1)',
+                backgroundColor: "rgba(101, 162, 178, 1)",
+                borderColor: "rgba(101, 162, 178, 1)",
             }
           ]
         },
@@ -207,22 +205,22 @@ $(function() {
     /////////////////////////////////////////////////////
 
     var myPieChart = new Chart(Piectx, {
-        type: 'doughnut',
+        type: "doughnut",
         data: {
             labels: ["Desktop", "Phones", "Tablets", "Consoles"],
             datasets: [{
                 data: [7798, 10828, 5420, 3425],
                 backgroundColor: [
-                    'rgba(90, 93, 167, 1)',
-                    'rgba(101, 162, 178, 1)',
-                    'rgba(118, 191, 122, 1)',
-                    'rgba(191, 120, 120, 1)'
+                    "rgba(90, 93, 167, 1)",
+                    "rgba(101, 162, 178, 1)",
+                    "rgba(118, 191, 122, 1)",
+                    "rgba(191, 120, 120, 1)"
                 ],
                 borderColor: [
-                    'rgba(90, 93, 167, 1)',
-                    'rgba(101, 162, 178, 1)',
-                    'rgba(118, 191, 122, 1)',
-                    'rgba(191, 120, 120, 1)'
+                    "rgba(90, 93, 167, 1)",
+                    "rgba(101, 162, 178, 1)",
+                    "rgba(118, 191, 122, 1)",
+                    "rgba(191, 120, 120, 1)"
                 ],
                 borderWidth: 1
             }]
@@ -240,22 +238,22 @@ $(function() {
     //  FORM VALIDATION
     /////////////////////////////////////////////////////
 
-    $('button.submit-btn').on('click', function(e) {
+    $("button.submit-btn").on("click", function(e) {
         // Stop Submit page refresh on click.
         e.preventDefault();
 
         // Variables for input fields
-        var $userInput = $('#userSearch');
-        var $msgUser = $('#msgToUser');
+        var $userInput = $("#userSearch");
+        var $msgUser = $("#msgToUser");
         //  Build msg for modal.
         var msg = "";
         //  Check if either input fields are empty.
         if ($msgUser.val() !== "" && $userInput.val() !== "") {
             // Get Msg recipent Name
             var user = $userInput.val();
-            msg += '<p>Your message to';
-            msg += '<br/><strong> ';
-            msg += user + ' </strong><br/>';
+            msg += "<p>Your message to";
+            msg += "<br/><strong> ";
+            msg += user + " </strong><br/>";
             msg += "has been sent!";
             //  Display msg in modal.
             displayModal(msg);
@@ -264,9 +262,9 @@ $(function() {
             //  clear textarea input.
             $msgUser.val("");
         } else {
-            msg += '<p>Please Complete the form with a ';
-            msg += '<br/><strong>User Name</strong>';
-            msg += ' and <strong>Message</strong></p>';
+            msg += "<p>Please Complete the form with a ";
+            msg += "<br/><strong>User Name</strong>";
+            msg += " and <strong>Message</strong></p>";
             displayModal(msg);
         }
         //  clear msg
@@ -282,43 +280,43 @@ $(function() {
     //  Check for localStorage Browser support
     if (typeof(Storage) !== "undefined") {
 
-        //  Sets users time zone from 'save' button.
+        //  Sets users time zone from "save" button.
         usersTimeZone(localStorage.timeZone);
 
         //  On load checks users localStorage settings
-        //  Applies Users settings from 'save' button.
+        //  Applies Users settings from "save" button.
         if (localStorage.emailSetting === "on") {
-            // Function call turns 'Send email Notifications' to 'on'.
+            // Function call turns "Send email Notifications" to "on".
             setEmailPref(localStorage.emailSetting);
         }
 
         if (localStorage.profileSetting === "on") {
-            // Function call turns 'Set Profile to Public' to 'on'.
+            // Function call turns "Set Profile to Public" to "on".
             setProfilePrivacy(localStorage.profileSetting);
         }
 
         ////////////////////////////////////////////
         //  Functions for Settings Widget
-        //  Functions called on 'save' button click.
-        //  Function called on page load if localStorage settings = 'on'.
+        //  Functions called on "save" button click.
+        //  Function called on page load if localStorage settings = "on".
         ////////////////////////////////////////////
 
 
         function setEmailPref(usersEmailPref) {
             if (usersEmailPref) {
-                $emailCheckbox.attr('checked', true);
+                $emailCheckbox.attr("checked", true);
             }
         }
 
         function setProfilePrivacy(usersProfilePref) {
             if (usersProfilePref) {
-                $privacyCheckbox.attr('checked', true);
+                $privacyCheckbox.attr("checked", true);
             }
         }
 
         function usersEmailPref() {
 
-            if ( $emailCheckbox.is(':checked') ) {
+            if ( $emailCheckbox.is(":checked") ) {
                 localStorage.emailSetting = "on";
             } else {
                 localStorage.emailSetting = "off";
@@ -328,7 +326,7 @@ $(function() {
 
         function usersProfilePref() {
 
-            if ( $privacyCheckbox.is(':checked') ) {
+            if ( $privacyCheckbox.is(":checked") ) {
                 localStorage.profileSetting = "on";
             } else {
                 localStorage.profileSetting = "off";
@@ -341,8 +339,8 @@ $(function() {
         //  Save Button Function
         ////////////////////////////////////////////
 
-        // Save App Sets 'click' Event
-        $("#saveBtn").on('click', function(e) {
+        // Save App Sets "click" Event
+        $("#saveBtn").on("click", function(e) {
             //  Store users time zone selection
             localStorage.setItem("timeZone", $("#timezone-select").val());
 
@@ -358,14 +356,25 @@ $(function() {
         console.log("Sorry, local storage is not available in your browser");
     }
 
-    //  Switches
-    $('#emailSwitch').on('change', function(e) {
-
+    //  Switches Hides <span>off</span> on change
+    $("#emailNotification").on("change", function(e) {
+      var $offSwitch = $("#emailOffSwitch");
+      if( $offSwitch.css("display") === "none") {
+        $offSwitch.show();
+      } else {
+        $offSwitch.hide();
+      }
     });
 
-    $('#privacySwitch').on('click', function(e) {
-
+    //  Switches Hides <span>off</span> on change
+    $("#profilePrivacy").on("click", function(e) {
+      var $offSwitch = $("#profileOffSwitch");
+      if( $offSwitch.css("display") === "none") {
+        $offSwitch.show();
+      } else {
+        $offSwitch.hide();
+      }
     });
 
 });
-//  End of iife function
+//  jQuery Ready();
